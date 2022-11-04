c5d109ff5f85bdd57d67634db5d22bdf17b6f592
James Scott <jsa.codes@gmail.com>
2022-11-04 10:01:46 -0500

    Created directories for game and event. Created files EventManager, Game Manager, deleted unnecessary ApplicationViews that was in components

--- src/views/ApplicationViews.js
+++ src/views/ApplicationViews.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom"
 import { Login } from "../components/auth/Login"
 import { Register } from "../components/auth/Register"
+import { EventList } from '../components/event/EventList'
 import { Authorized } from "./Authorized"
 
 
@@ -10,7 +11,7 @@ export const ApplicationViews = () => {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route element={<Authorized />}>
-                {/* Add Routes here */}
+                <Route path="/" element={<EventList />} />
             </Route>
         </Routes>
     </>
