e2ba81c932d40b9c07f831c397eae96cc8aabf68
James Scott <jsa.codes@gmail.com>
2022-11-04 10:44:42 -0500

    Added the appropriate URL to the fetch calls with the correct resource after the / for both createGame and getGameTypes. Also added the headers to handle the authorization, and the response.json. Also created a GameForm component in the game module.

--- src/managers/GameManager.js
+++ src/managers/GameManager.js
@@ -8,11 +8,19 @@ export const getGames = () => {
 }
 
 export const createGame = (game) => {
-    return fetch("", { })
+    return fetch("http://localhost:8000/games", { 
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
         .then()
 }
 
-export const getGameTypes = () => {
-    return fetch("", { })
-        .then()
+export const getGameTypes = (gameType) => {
+    return fetch("http://localhost:8000/gameTypes", { 
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
+        .then(response => response.json())
 }
